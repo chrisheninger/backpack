bd5130c99c295bc840413274e6e12a5433836d73
Chris Heninger <chris@igravitystudios.com>
2022-07-15 20:36:53 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ch/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install git node yarn
@@ -12,14 +12,13 @@ brew install graphicsmagick
 brew install --cask 1password
 brew install --cask adobe-creative-cloud
 brew install --cask aerial
-brew install --cask alfred
 brew install --cask authy
 brew install --cask firefox
 brew install --cask google-chrome
 brew install --cask google-drive
 brew install --cask iterm2
-brew install --cask kap
 brew install --cask postico
+brew install --cask raycast
 brew install --cask slack
 brew install --cask spotify
 brew install --cask visual-studio-code
@@ -35,4 +34,4 @@ defaults write com.apple.screencapture location ~/Screenshots
 killall SystemUIServer
 
 # Last
-source ~/.bash_profile
+source ~/.zprofile
