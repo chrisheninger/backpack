83f7ad71441775ed3eedd68c1ae397350cd2339e
Chris Heninger <chris@igravitystudios.com>
2025-05-22 08:10:29 -0700

    2025

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install git node yarn
 brew install android-platform-tools
-brew install graphicsmagick
+
+yes '' | /bin/bash -c "$(curl -fsSL https://bun.sh/install)"
 
 # Casks
 brew install --cask 1password
-brew install --cask adobe-creative-cloud
 brew install --cask aerial
 brew install --cask authy
-brew install --cask firefox
+brew install --cask cursor
 brew install --cask google-chrome
 brew install --cask google-drive
 brew install --cask iterm2
@@ -21,7 +21,6 @@ brew install --cask postico
 brew install --cask raycast
 brew install --cask slack
 brew install --cask spotify
-brew install --cask visual-studio-code
 brew install --cask vlc
 
 # Copy dotfiles to user's home directory
